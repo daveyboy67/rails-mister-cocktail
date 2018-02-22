ab35ec44fea985baf52211fa8a2cff1f99ddaf5d
David O'Neill daveyoneill123@gmail.com <daveyoneill123@gmail.com>
2018-02-22 15:55:37 +0000

    mister cocktail half way

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p Ingredient.all
+Ingredient.destroy_all
+p Ingredient.all
+
+ingredients = %w(lemon ice mint lime kiwi milk sugar vodka whiskey orange gin brandy)
+
+ingredients.each do |ingredient|
+  p Ingredient.create(name: ingredient)
+end
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "lime")
+# Ingredient.create(name: "kiwi")
+# Ingredient.create(name: "milk")
+# Ingredient.create(name: "sugar")
+# Ingredient.create(name: "vodka")
+# Ingredient.create(name: "whiskey")
+# Ingredient.create(name: "orange")
+# Ingredient.create(name: "gin")
+# Ingredient.create(name: "brandy")
+# Ingredient.create(name: "barcardi")
+# Ingredient.create(name: "campari")
+# Ingredient.create(name: "amaretto")
+# Ingredient.create(name: "scotch")
+# Ingredient.create(name: "black sambuca")
+# Ingredient.create(name: "egg yolk")
+
+puts 'Created x amount of ingredients'
