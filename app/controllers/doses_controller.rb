8be9d017c1cea5fb02a60f94d9ea352753bc117a
David O'Neill daveyoneill123@gmail.com <daveyoneill123@gmail.com>
2018-02-23 16:17:37 +0000

    Prepare for Heroku

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,46 @@
 class DosesController < ApplicationController
+
+  def index
+    @doses = Doses.all
+  end
+
+  def show
+    @dose = Dose.find(params[:id])
+
+  end
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+
+  def create
+    dose = Dose.new(dose_strong_params)
+    dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if dose.save
+      redirect_to cocktail_path(dose.cocktail)
+    else
+      render :new
+    end
+  end
+
+
+
+  def destroy
+    dose = Dose.find(params[:id])
+    cocktail = dose.cocktail
+    dose.destroy
+    redirect_to cocktail_path(cocktail)
+  end
+
+
+
+     private
+
+  def dose_strong_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :cocktails, :only => [ :index, :show, :new, :create ]
+  resources :cocktails, only: [ :index, :show, :new, :create ] do
+  resources :doses, only: [:new, :index, :create ]
+end
+
+resources :doses, only: [:destroy]
+root to: 'cocktails#index'
 end
 
